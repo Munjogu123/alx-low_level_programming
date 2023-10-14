37fd2bab2e95fb245317ed6d3ea4358b2d580135
Munjogu123 <fredmunjogu@gmail.com>
2023-10-14 12:02:02 +0000

    print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
 /**
+ * print_times_table - prints the n times table
+ * @n: operand
  */
 void print_times_table(int n)
 {
@@ -25,12 +27,6 @@ void print_times_table(int n)
 					_putchar(k + '0');
 					_putchar(',');
 					_putchar(' ');
-				} else if (j < n && k > 9)
-				{
-					_putchar(tens_1 + '0');
-					_putchar(ones + '0');
-					_putchar(',');
-					_putchar(' ');
 				} else if (j < n && k > 99)
 				{
 					_putchar(hundreds + '0');
@@ -38,21 +34,27 @@ void print_times_table(int n)
 					_putchar(ones + '0');
 					_putchar(',');
 					_putchar(' ');
-				}else if (j == n && k > 9)
+				} else if (j < n && k > 9)
 				{
 					_putchar(tens_1 + '0');
 					_putchar(ones + '0');
+					_putchar(',');
+					_putchar(' ');
 				} else if (j == n && k > 99)
 				{
 					_putchar(hundreds + '0');
 					_putchar(tens_2 + '0');
 					_putchar(ones + '0');
-				}else
+				} else if (j == n && k > 9)
+				{
+					_putchar(tens_1 + '0');
+					_putchar(ones + '0');
+				} else
 				{
 					_putchar(k + '0');
 				}
 			}
 			_putchar('\n');
 		}
-	}	
+	}
 }
