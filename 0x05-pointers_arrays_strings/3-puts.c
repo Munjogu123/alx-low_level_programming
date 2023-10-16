8377715ebd936d2f7fd3a8a58cdc3c3223142251
Munjogu123 <fredmunjogu@gmail.com>
2023-10-16 18:22:19 +0000

    prints a string, followed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,5 +6,23 @@
  */
 void _puts(char *str)
 {
-	puts(str);
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
 }
