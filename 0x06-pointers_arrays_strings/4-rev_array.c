c096b620e524bf0c360a0e9d4a83a3c0b6eae6a1
Munjogu123 <fredmunjogu@gmail.com>
2023-10-21 02:37:35 +0000

    reverses the content of an array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,42 +8,12 @@
 void reverse_array(int *a, int n)
 {
 	int i;
+	int j;
 
-	for (i = n - 1; i >= 0; i--)
+	for (i = 0; i < n--; i++)
 	{
-		if (i > 0)
-		{
-			if (a[i] <= 9)
-			{
-				_putchar(a[i] + '0');
-				_putchar(',');
-				_putchar(' ');
-			} else if (a[i] <= 99)
-			{
-				_putchar((a[i] / 10) + '0');
-				_putchar((a[i] % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			} else if (a[i] <= 999)
-			{
-				_putchar((a[i] / 100) + '0');
-				_putchar((a[i] % 10) / 10 + '0');
-				_putchar((a[i] % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			} else if (a[i] <= 9999)
-			{
-				_putchar((a[i] / 1000) + '0');
-				_putchar((a[i] % 1000) / 100 + '0');
-				_putchar((a[i] % 100) / 10 + '0');
-				_putchar((a[i] % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-		} else
-		{
-			_putchar(a[i] + '0');
-		}
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
 	}
-	_putchar('\n');
 }
