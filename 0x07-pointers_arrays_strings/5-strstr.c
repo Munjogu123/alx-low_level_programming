7e0d641aac39b8b538933e941a70e96ca287eb15
Munjogu123 <fredmunjogu@gmail.com>
2023-10-23 17:29:52 +0000

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strstr - locates a substring
+ * @haystack: source string
+ * @needle: search string
+ *
+ * Return: return substring that matches. Otherwise NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
+}
