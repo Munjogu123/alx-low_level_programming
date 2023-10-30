4876b12a518639a7bf64053318dd943468a69eee
Munjogu123 <fredmunjogu@gmail.com>
2023-10-30 17:20:04 +0000

    returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string to be replaced
+ *
+ * Return: NULL if str equals to NULL. Otherwise replaced string
+ */
+char *_strdup(char *str)
+{
+	char *replace;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	replace = (char *)malloc(sizeof(char) * _strlen(str));
+	for (i = 0; str[i] != '\0'; i++)
+		replace[i] = str[i];
+
+	*(replace + i) = '\0';
+	return (replace);
+}
