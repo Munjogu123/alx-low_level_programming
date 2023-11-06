8d9ee0147e130f6efc0fe148f95246eded004757
Munjogu123 <fredmunjogu@gmail.com>
2023-11-06 03:00:52 -0800

    creates a struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - contains the characteristics of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog's owner
+ */
 struct dog
 {
 	char *name;
