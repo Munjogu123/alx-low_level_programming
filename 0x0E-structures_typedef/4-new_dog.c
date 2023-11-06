8c16b4306dd577dad3ab8909bd2900501087e2f5
Munjogu123 <fredmunjogu@gmail.com>
2023-11-06 16:09:16 +0000

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,6 @@
-#include "main.h"
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * _strlen - function that returns the length of a string
@@ -20,10 +22,69 @@ int _strlen(char *s)
 }
 
 /**
+ * _strcpy - copies src to dest
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: returns pointer to copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - new dog created
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: returns the elements of the type
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *s;
+	int len1, len2;
+	dog_t *scooby;
 
-	s = malloc(sizeof(char) * (_strlen(owner) + 1));
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	scooby = malloc(sizeof(dog_t));
+	if (scooby == NULL)
+		return (NULL);
+
+	scooby->name = malloc(sizeof(char) * (len1 + 1));
+	if (scooby->name == NULL)
+	{
+		free(scooby);
+		return (NULL);
+	}
+
+	scooby->owner = malloc(sizeof(char) * (len2 + 1));
+	if (scooby->owner == NULL)
+	{
+		free(scooby);
+		free(scooby->name);
+		return (NULL);
+	}
+
+	scooby->age = age;
+	_strcpy(scooby->name, name);
+	_strcpy(scooby->owner, owner);
+	return (scooby);
 }
