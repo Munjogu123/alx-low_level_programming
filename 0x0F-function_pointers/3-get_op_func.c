c3409f33210cb3540a1fa6e0bde7a87db3a857be
Munjogu123 <fredmunjogu@gmail.com>
2023-11-08 18:47:34 +0000

    gets correct function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function
+ * @s: arithmetic operator
+ *
+ * Return: returns the correct function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	i = 0;
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (0);
+}
