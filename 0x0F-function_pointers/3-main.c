2fb395f4ed38f69b54485821a579149d21663f2f
Munjogu123 <fredmunjogu@gmail.com>
2023-11-08 18:46:27 +0000

    adds main file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns result of operations
+ * @argc: no of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int (*result)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	result = get_op_func(argv[2]);
+
+	if (result == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", result(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
