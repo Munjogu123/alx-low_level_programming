82cb8242001a1464e607a9815c9d3cae8652feac
Munjogu123 <fredmunjogu@gmail.com>
2023-11-20 02:22:47 -0800

    inserts a new node at a given position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *ptr = malloc(sizeof(listint_t));
+	listint_t *temp = *head;
+
+	if (!ptr)
+		return (NULL);
+
+	ptr->n = n;
+
+	if (*head == NULL)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		ptr->next = *head;
+		*head = ptr;
+		return (ptr);
+	}
+
+	while (*head != NULL)
+	{
+		if (i == idx - 1)
+		{
+			ptr->next = temp->next;
+			temp->next = ptr;
+			return (ptr);
+		}
+		temp = temp->next;
+		i++;
+	}
+
+	return (NULL);
+}
