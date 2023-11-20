7cca8a222bcede0622f926dbf2349a322050a553
Munjogu123 <fredmunjogu@gmail.com>
2023-11-20 17:34:45 +0000

    inserts a new node at a given position

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		current = NULL;
 		return (1);
 	}
-	while (index != 1)
+	while (index != 0)
 	{
 		previous = current;
 		current = current->next;
