a3dcfad07fc7dd25c3f942c50ddfd6e6f20838d0
Munjogu123 <fredmunjogu@gmail.com>
2023-12-13 21:21:34 -0800

    checks the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness -  checks the endianness
+ *
+ * Return: if endian 1 otherwise 0.
+ */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *end = (char *)&test;
+
+	if (*endian)
+		return (1);
+
+	return (0);
+}
