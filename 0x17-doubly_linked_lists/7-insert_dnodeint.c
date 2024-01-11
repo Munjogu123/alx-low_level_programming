d6804b9bba31ce082714e706e2c2f3788304ec90
Munjogu123 <fredmunjogu@gmail.com>
2024-01-11 21:01:31 +0300

    inserts node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,11 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		prev = prev->next;
 		count++;
 	}
-	if (prev == NULL)
-	{
-		new = add_dnodeint(h, n);
-	}
-	else if (prev->next == NULL)
+	if (prev->next == NULL)
 	{
 		new = add_dnodeint_end(h, n);
 	} else
