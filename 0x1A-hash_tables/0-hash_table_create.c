5b5bb48a6a0a3392a82e9b7dbae0683242f600f8
Munjogu123 <fredmunjogu@gmail.com>
2024-02-02 00:05:27 +0300

    creates hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,18 +9,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new = malloc(sizeof(hash_table_t));
-	unsigned long int i;
 
 	if (new == NULL)
 		return (NULL);
 
 	new->size = size;
-	new->array = malloc(new->size);
+	new->array = malloc(new->size * sizeof(hash_node_t *));
 	if (new->array == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-		new->array[i] = NULL;
-
-		return (new);
+	return (new);
 }
